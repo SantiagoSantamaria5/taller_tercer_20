b195699712ca3b095e6ff792652d2a3cf8a1e68e
SantiagoSantamaria5 <sasantamaria@uniboyaca.edu.co>
2023-10-05 20:23:32 -0500

    creacion de carpetas

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>menu</title>
+</head>
+<body>
+    <div>
+        <a>Primer ejercicio</a>
+    </div>
+</body>
+</html>
