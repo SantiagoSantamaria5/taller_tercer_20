600ca1b2f5c8877b02d34d04b568e9bd31b83fd4
SantiagoSantamaria5 <sasantamaria@uniboyaca.edu.co>
2023-10-14 10:10:29 -0500

    cambios en tecer punto

--- controllers/notas.php
+++ controllers/notas.php
@@ -1,29 +1,60 @@
 <?php
+
+$calificacion_maxima = '';
+$calificacion_minima = '';
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    
-    $cantidad_notas = (isset($_POST['cantidad_notas']) && is_numeric($_POST['cantidad_notas'])) ? intval($_POST['cantidad_notas']) : 0;
+
     $nombre_materia = $_POST['nombre_materia'] ?? '';
+    $cantidad_notas = (isset($_POST['cantidad_notas']) && is_numeric($_POST['cantidad_notas'])) ? intval($_POST['cantidad_notas']) : 0;
     $calificacion_minima = (isset($_POST['calificacion_minima']) && is_numeric($_POST['calificacion_minima'])) ? floatval($_POST['calificacion_minima']) : 0;
     $calificacion_maxima = (isset($_POST['calificacion_maxima']) && is_numeric($_POST['calificacion_maxima'])) ? floatval($_POST['calificacion_maxima']) : 0;
 
     $suma_notas = 0;
     $error = false;
 
-    for ($i = 1; $i <= $cantidad_notas; $i++) {
-        $nota = (isset($_POST['nota' . $i]) && is_numeric($_POST['nota' . $i])) ? floatval($_POST['nota' . $i]) : 0;
-
-        if ($nota < $calificacion_minima || $nota > $calificacion_maxima) {
-            echo "La nota $i no está dentro del rango de calificaciones.";
-            $error = true;
-            break;
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cantidad_notas"])) {
+        $cantidad_notas = intval($_POST["cantidad_notas"]);
+        
+        if ($cantidad_notas > 0) {
+            echo '<form method="post">';
+            for ($i = 1; $i <= $cantidad_notas; $i++) {
+                echo '<label for="nota' . $i . '">Nota ' . $i . ':</label>';
+                echo '<input type="text" name="nota' . $i . '" id="nota' . $i . '"><br>';
+            
+            }
+            echo '<input type="submit" value="Calcular">';
+            echo '</form>';
+            echo "la calificacion minima es :" ,$calificacion_minima = $_POST['calificacion_minima'];
+            echo "<br>";
+            echo "la calificacion maxima es :" , $calificacion_maxima = $_POST['calificacion_maxima'];
+           
         } else {
-            $suma_notas += $nota;
+            echo "La cantidad de notas debe ser mayor que cero.";
         }
     }
-
-    if (!$error) {
-        $promedio = ($cantidad_notas > 0) ? ($suma_notas / $cantidad_notas) : 0;
-        echo "El promedio de la materia $nombre_materia es: $promedio";
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["nota1"])) {
+        $suma_notas = 0;
+        $error = false;
+        $calificacion_minima = $_POST['calificacion_minima'] ;
+        $calificacion_maxima = $_POST['calificacion_maxima'] ;
+    
+        for ($i = 1; $i <= $cantidad_notas; $i++) {
+            $nota = isset($_POST['nota' . $i]) && is_numeric($_POST['nota' . $i]) ? floatval($_POST['nota' . $i]) : 0;
+    
+            if ($nota < $calificacion_minima || $nota > $calificacion_maxima) {
+                echo "La nota $i no está dentro del rango de calificaciones.";
+                $error = true;
+                break;
+            } else {
+                $suma_notas += $nota;
+            }
+        }
+    
+        if (!$error) {
+            echo "La suma de las notas es: $suma_notas";
+        }
     }
 }
-?>
+ ?>
